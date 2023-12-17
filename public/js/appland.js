4c491c17769fd45bbc5644826fbfb9619dc92b40
AnikHaque <ekramul15-811@diu.edu.bd>
2023-12-18 04:26:50 +0600

    some comment deletion in appland.js file

--- public/js/appland.js
+++ public/js/appland.js
@@ -1,16 +1,8 @@
-/**
-* Template Name: Appland
-* Updated: May 30 2023 with Bootstrap v5.3.0
-* Template URL: https://bootstrapmade.com/free-bootstrap-app-landing-page-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
-  /**
-   * Easy selector helper function
-   */
+  
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
@@ -20,9 +12,7 @@
     }
   }
 
-  /**
-   * Easy event listener function
-   */
+ 
   const on = (type, el, listener, all = false) => {
     let selectEl = select(el, all)
     if (selectEl) {
@@ -34,16 +24,12 @@
     }
   }
 
-  /**
-   * Easy on scroll event listener 
-   */
+  
   const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener)
   }
 
-  /**
-   * Navbar links active state on scroll
-   */
+  
   let navbarlinks = select('#navbar .scrollto', true)
   const navbarlinksActive = () => {
     let position = window.scrollY + 200
@@ -61,9 +47,7 @@
   window.addEventListener('load', navbarlinksActive)
   onscroll(document, navbarlinksActive)
 
-  /**
-   * Scrolls to an element with header offset
-   */
+ 
   const scrollto = (el) => {
     let header = select('#header')
     let offset = header.offsetHeight
@@ -74,9 +58,7 @@
     })
   }
 
-  /**
-   * Toggle .header-scrolled class to #header when page is scrolled
-   */
+ 
   let selectHeader = select('#header')
   if (selectHeader) {
     const headerScrolled = () => {
@@ -90,9 +72,7 @@
     onscroll(document, headerScrolled)
   }
 
-  /**
-   * Back to top button
-   */
+  
   let backtotop = select('.back-to-top')
   if (backtotop) {
     const toggleBacktotop = () => {
@@ -106,18 +86,14 @@
     onscroll(document, toggleBacktotop)
   }
 
-  /**
-   * Mobile nav toggle
-   */
+ 
   on('click', '.mobile-nav-toggle', function(e) {
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
   })
 
-  /**
-   * Mobile nav dropdowns activate
-   */
+  
   on('click', '.navbar .dropdown > a', function(e) {
     if (select('#navbar').classList.contains('navbar-mobile')) {
       e.preventDefault()
@@ -125,9 +101,7 @@
     }
   }, true)
 
-  /**
-   * Scrool with ofset on links with a class name .scrollto
-   */
+ 
   on('click', '.scrollto', function(e) {
     if (select(this.hash)) {
       e.preventDefault()
@@ -143,9 +117,7 @@
     }
   }, true)
 
-  /**
-   * Scroll with ofset on page load with hash links in the url
-   */
+ 
   window.addEventListener('load', () => {
     if (window.location.hash) {
       if (select(window.location.hash)) {
@@ -154,9 +126,7 @@
     }
   });
 
-  /**
-   * Gallery Slider
-   */
+  
   new Swiper('.gallery-slider', {
     speed: 400,
     loop: true,
@@ -191,9 +161,7 @@
     }
   });
 
-  /**
-   * Initiate gallery lightbox 
-   */
+  
   const galleryLightbox = GLightbox({
     selector: '.gallery-lightbox'
   });
